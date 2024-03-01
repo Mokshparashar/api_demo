82bc3dc00a51bf8641ab90e70c804034388dcdc0
Moksh parashar <parasharm239@gmail.com>
2024-03-01 05:01:23 -0800

    .

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import { data } from "./data.js";
 import express from "express";
+import cors from "cors";
 const port = 3000;
 const app = express();
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
 
+app.use(cors(corsOptions));
 app.get("/", (req, res) => {
   res.json(data);
 });
